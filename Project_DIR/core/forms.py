f7fad93db13ddc1c698e8dd89ca93fb129f50a21
Avinash Bharti <avinashbharti97@gmail.com>
2018-06-19 19:01:08 +0530

    first and last name option in registration added and crispy forms installed

--- Project_DIR/core/forms.py
+++ Project_DIR/core/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
