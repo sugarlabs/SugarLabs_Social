3dbcc7a693ee3872208153880c8430d33550e6b5
Avinash Bharti <avinashbharti97@gmail.com>
2018-06-21 19:12:43 +0530

    Post model created and registered with admin

--- Project_DIR/post/admin.py
+++ Project_DIR/post/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
+    list_display = ('title', 'created_at', 'author')
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
--- Project_DIR/post/models.py
+++ Project_DIR/post/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
-    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
