e9e3bca56bf9d000f60cf2f38de6267dd0609b75
Avinash Bharti <avinashbharti97@gmail.com>
2018-07-11 05:29:01 +0530

    left nav button class toggling on active

--- Project_DIR/static/script.js
+++ Project_DIR/static/script.js
@@ -1,29 +1,11 @@
-// $(document).ready(function(){
-//   $("responsive-img img").each(function(){
-//     $(this).addClass("img-fluid")
-//   });
-// });
 
+//to toggle active class of left menu with current page
+$(document).ready(function () {
+    var url = window.location;
+    $('.left-nav-btn-div a[href="'+ url +'"]').parent().addClass('left-nav-active');
+    $('.left-nav-btn-div a').filter(function() {
+         return this.href == url;
+    }).parent().addClass('left-nav-active');
+    console.log("hey");
 
-
-//for drag and drop attachment of images in markdown editor
-
-inlineAttachment.editors.codemirror4.attach(simplemde.codemirror, {
-    onFileUploadResponse: function(xhr) {
-        var result = JSON.parse(xhr.responseText),
-        filename = result[this.settings.jsonFieldName];
-
-        if (result && filename) {
-            var newValue;
-            if (typeof this.settings.urlText === 'function') {
-                newValue = this.settings.urlText.call(this, filename, result);
-            } else {
-                newValue = this.settings.urlText.replace(this.filenameTag, filename);
-            }
-            var text = this.editor.getValue().replace(this.lastValue, newValue);
-            this.editor.setValue(text);
-            this.settings.onFileUploaded.call(this, filename);
-        }
-        return false;
-    }
 });
