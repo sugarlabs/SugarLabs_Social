b4f0cb030d2587ebf507ca97a6d8e9c2142b93bb
avinashbharti97 <avinashbharti97@gmail.com>
2018-08-27 23:51:03 +0530

    software models created

--- Project_DIR/softwares/models.py
+++ Project_DIR/softwares/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.utils.html import mark_safe
+from markdown import markdown
 
 # Create your models here.
+class Software(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+    WEB = 'web'
+    SUGAR = 'sugar'
+    UBUNTU = 'ubuntu'
+    FEDORA = 'fedora'
+    WINDOWS = 'windows'
+    CHROME = 'chrome'
+    ANDROID = 'android'
+    categories_choices = (
+        (WEB = 'web')
+        (SUGAR = 'sugar')
+        (UBUNTU = 'ubuntu')
+        (FEDORA = 'fedora')
+        (WINDOWS = 'windows')
+        (CHROME = 'chrome')
+        (ANDROID = 'android')
+    )
+    
+    category = models.CharField(
+        max_length=10,
+        choices = categories_choices,
+        default = NA
+    )
+
+    software_source = models.URLField(max_length = 200)
+    info = models.TextField(max_length=200000)
+    installation_guide = models.TextField(max_length=200000)
+    contribution_guide = models.TextField(max_length=200000)
+
+
+    def __str__(self):
+        return self.title
+
+    def get_content_as_markdown(self):
+        return mark_safe(markdown(self.info, safe_mode='escape'))
+
+    def get_content_as_markdown(self):
+        return mark_safe(markdown(self.installation_guide, safe_mode='escape'))
+        
+    def get_content_as_markdown(self):
+        return mark_safe(markdown(self.contribution_guide, safe_mode='escape'))
+        
+
