80167d114c0754de82b582a11188438c993928e0
avinashbharti97 <avinashbharti97@gmail.com>
2018-08-27 23:57:41 +0530

    softwares view created

--- Project_DIR/softwares/views.py
+++ Project_DIR/softwares/views.py
@@ -2,13 +2,18 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import Context, loader
 from django.urls import reverse
+from .models import Software
 
 # Create your views here.
 def softwares(request):
     username = request.user.username
+    all_softwares = Software.objects.all()
     context_dict = {'username':username,
+                    'softwares':all_softwares
     }
 
+    for software in all_softwares:
+        software.url = software.title.replace(' ', '_')
 
     return render(request, 'core/softwares.html', context_dict)
 
