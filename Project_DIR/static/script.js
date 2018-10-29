75bcd71513dde4aa39021537c5f45976173e6dbf
Freddie Nicholson <freddie@fr3ddie.me>
2018-10-29 22:05:14 +0000

    viewer.js for zoomable images

--- Project_DIR/static/script.js
+++ Project_DIR/static/script.js
@@ -11,5 +11,43 @@ $(document).ready(function () {
     $('.left-nav-btn-div a').filter(function() {
          return this.href == url;
     }).parent().addClass('left-nav-active');
+//any images with the zoomimg will be zoomed in using viewer js when clicked
+    $( ".zoomimg" ).click(function(e) {
+        var viewer = new Viewer(e.toElement, {
+          button: true,
+          transition: true,
+          backdrop: true,
+          navbar: false,
+          toolbar: false,
+          title: false,
+          tooltip: false,
+          hidden: function () {
+            viewer.destroy();
+          },
+        });
+
+        // image.click();
+        viewer.show();
+      });
+      $( ".pzoomimg" ).click(function(e) {
+        var img = new Image();
+        img.src = (e.toElement.style.backgroundImage.split("\""))[1];
+        console.log(img.src);
+        var viewer = new Viewer(img, {
+          button: true,
+          transition: true,
+          backdrop: true,
+          navbar: false,
+          toolbar: false,
+          title: false,
+          tooltip: false,
+          hidden: function () {
+            viewer.destroy();
+          },
+        });
+
+        // image.click();
+        viewer.show();
+      });
 
 });
