c095095d926d4d6be2b29e09ef269204234b0616
Freddie Nicholson <freddie@fr3ddie.me>
2018-11-01 19:53:51 +0000

    Revert "implemented navbar fixed scrolling"
    This reverts commit afec0c52da42d721645105d8aa8c76759db2d3a1.

--- Project_DIR/static/script.js
+++ Project_DIR/static/script.js
@@ -11,20 +11,43 @@ $(document).ready(function () {
     $('.left-nav-btn-div a').filter(function() {
          return this.href == url;
     }).parent().addClass('left-nav-active');
-    
-    checkNav()
-    $(window).scroll(function () {
-        checkNav()
+//any images with the zoomimg will be zoomed in using viewer js when clicked
+    $( ".zoomimg" ).click(function(e) {
+        var viewer = new Viewer($(this).get(0), {
+          button: true,
+          transition: true,
+          backdrop: true,
+          navbar: false,
+          toolbar: false,
+          title: false,
+          tooltip: false,
+          hidden: function () {
+            viewer.destroy();
+          },
         });
-    function checkNav() {
-        if($(window).scrollTop() >= 60) {
-            $(".left-nav").css("margin-top", "-5%");
-            $(".left-nav").css("padding-top", "3%")
-            $(".btn-add-left-nav").css("margin-top", "50px")
-        } else {
-            $(".left-nav").css("margin-top", "0");
-            $(".left-nav").css("padding-top", "0")
-            $(".btn-add-left-nav").css("margin-top", "0")
-        }
-    }
+
+        // image.click();
+        viewer.show();
+      });
+      $( ".pzoomimg" ).click(function(e) {
+        var img = new Image();
+        img.src = ($(this).get(0).style.backgroundImage.split("\""))[1];
+        console.log(img.src);
+        var viewer = new Viewer(img, {
+          button: true,
+          transition: true,
+          backdrop: true,
+          navbar: false,
+          toolbar: false,
+          title: false,
+          tooltip: false,
+          hidden: function () {
+            viewer.destroy();
+          },
+        });
+
+        // image.click();
+        viewer.show();
+      });
+
 });
